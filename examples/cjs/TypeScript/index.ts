fd9c96da4d0c6d4dd96e8d9a93519279261f899d
CarterCobb <carter.cobb72@gmail.com>
2022-04-26 12:35:55 -0600

    added cjs TS example

---
+++ examples/cjs/TypeScript/index.ts
@@ -0,0 +1,14 @@
+import { RouteFactory, RouteFactoryOptions } from "node-server-router";
+import express, { Application } from "express";
+
+const app: Application = express();
+
+const config: RouteFactoryOptions = {
+  api_version: "/v1",
+  log_configured: true,
+  route_dir: "routes",
+};
+
+RouteFactory.applyRoutesTo(app, config);
+
+app.listen(6969, () => console.log("started"));
