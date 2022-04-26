edad241cd3ed685a650db640e690f6f21c1f27de
CarterCobb <carter.cobb72@gmail.com>
2022-04-26 00:22:24 -0600

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import RouteFactory from './models/RouteFactory.js';
-import { HTTPAction } from './types/Route.js';
+import { HTTPAction, Route, Routes } from './types/Route.js';
+import RouteFactoryOptions from './types/RouteFactoryOptions.js';
 
-export { HTTPAction, RouteFactory };
+export { HTTPAction, RouteFactory, Route, Routes, RouteFactoryOptions };
